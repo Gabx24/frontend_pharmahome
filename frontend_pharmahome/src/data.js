61a3f1f5e28f191dc014160788aaa4882361cee5
Gabriela <g.negran@yahoo.com>
2022-09-19 13:51:56 +0200

    landing page

---
+++ frontend_pharmahome/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+  products: [
+    {
+      name: "The Ordinary Niacinamide",
+      slug: "the-ordinary-niacinamide",
+      category: "Cosmetics",
+      image: "/images/p1.jpg",
+      price: 7.5,
+      countInStock: 10,
+      brand: "The ordinary",
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "Our Niancimide 10% + 1 % Zinc formula is a water- based serum that boosts skin brightness, improved skin smoothness and reinforces the skin barrier over time. ",
+    },
+    {
+      name: "Imodium",
+      slug: "imodium-medication",
+      category: "Medication",
+      image: "/images/p2.jpg",
+      price: 7,
+      countInStock: 50,
+      brand: "Imodium",
+      rating: 4,
+      numReviews: 15,
+      description:
+        "Loperamide is a medicine to treat diarrhoea. It can help with short-term diarrhoea or irritable bowel syndrome (IBS).",
+    },
+    {
+      name: "Cerave Foaming Facial Cleanser",
+      slug: "cerave-foaming-facial-cleanser",
+      category: "Cosmetics",
+      image: "/images/p3.jpg",
+      price: 12,
+      countInStock: 80,
+      brand: "CERAVE",
+      rating: 5,
+      numReviews: 25,
+      description:
+        "CeraVe Foaming Facial Cleanser features ceramides, hyaluronic acid, and niacinamide, and is formulated to help to maintain your skin's protective barrier.",
+    },
+    {
+      name: "Talcid",
+      slug: "talcid-tablets",
+      category: "Medication",
+      image: "/images/p4.jpg",
+      price: 15,
+      countInStock: 75,
+      brand: "Talcid",
+      rating: 4,
+      numReviews: 8,
+      description:
+        "Talcid Tablet is used for relieving acidity and heartburn in conditions such as. It contains rabeprazole as its active medicine.",
+    },
+  ],
+};
+
+export default data;
