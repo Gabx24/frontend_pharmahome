c5793bef85e2099bad427f5d3b02ba33297f352f
Gabriela <g.negran@yahoo.com>
2022-09-21 16:27:45 +0200

    deleted merge conflict tags

--- frontend_pharmahome/src/App.js
+++ frontend_pharmahome/src/App.js
@@ -1,10 +1,7 @@
 import data from "./data";
-<<<<<<< HEAD
+
 import Footer from "./Components/Footer";
-import FeatureProduct from "./Components/FeatureProduct";
-import ControlledCarousel from "./Components/Carousel";
-import "bootstrap/dist/css/bootstrap.min.css";
-=======
+
 //import logo from "./logo.png";
 import FeatureProduct from "./Components/FeatureProduct";
 import ControlledCarousel from "./Components/Carousel";
@@ -15,13 +12,10 @@ import Cart from "./Components/Cart";
 import Dashboard from "./Components/Dashboard";
 import Profile from "./Components/Profile";
 import OrderHistory from "./Components/OrderHistory.js";
->>>>>>> b70827ff38ad13d7d9f506ad5953291c653bc1ef
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-=======
       <Router basename="/">
         <Routes>
           <Route exact path="/" component={Home} />
@@ -31,25 +25,16 @@ function App() {
           <Route path="/OrderHistory" component={OrderHistory} />
         </Routes>
       </Router>
->>>>>>> b70827ff38ad13d7d9f506ad5953291c653bc1ef
-      {/* <header>
-        <img src={logo} className="logo-header" alt="logo" />
-        <a href="/">PharmaHome</a>
-      </header> */}
+
       <main>
         <ControlledCarousel />
         <h1>Featured Products</h1>
         <div className="products">
           {data.products.map((product) => (
-<<<<<<< HEAD
-            
-            
-=======
             <FeatureProduct product={product} />
->>>>>>> b70827ff38ad13d7d9f506ad5953291c653bc1ef
           ))}
         </div>
-        <Footer/>
+        <Footer />
       </main>
     </div>
   );
